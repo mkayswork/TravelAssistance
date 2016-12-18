d3646829c37d5f06db8e31ad8a8528b099fd0b8a
Matthias Kühnel <Mkayswork@gmail.com>
2016-12-18 19:06:17 +0100

    Initial setup for CocoaPods

---
+++ DB Travel Assistance/Pods/Target Support Files/Pods-DB Travel Assistance/Pods-DB Travel Assistance-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DB_Travel_Assistance : NSObject
+@end
+@implementation PodsDummy_Pods_DB_Travel_Assistance
+@end
---
+++ DB Travel Assistance/Pods/Target Support Files/Pods-DB Travel Assistance/Pods-DB Travel Assistance-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_DB_Travel_AssistanceVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DB_Travel_AssistanceVersionString[];
+
